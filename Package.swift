22ebeb9ab4407f856c1dc1b8aff4eb8fad5ca8c0
yukiny0811 <yukiny0811@gmail.com>
2025-05-02 18:17:14 +0900

    fix Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,6 @@ let package = Package(
     name: "OpenAIStructure",
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "OpenAIStructure",
             targets: ["OpenAIStructure"]
@@ -22,9 +21,6 @@ let package = Package(
         .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        // Macro implementation that performs the source transformation of a macro.
         .macro(
             name: "OpenAIStructureMacros",
             dependencies: [
@@ -32,12 +28,7 @@ let package = Package(
                 .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
             ]
         ),
-
-        // Library that exposes a macro as part of its API, which is used in client programs.
         .target(name: "OpenAIStructure", dependencies: ["OpenAIStructureMacros"]),
-
-        // A client of the library, which is able to use the macro in its own code.
         .executableTarget(name: "OpenAIStructureClient", dependencies: ["OpenAIStructure"]),
-
     ]
 )
